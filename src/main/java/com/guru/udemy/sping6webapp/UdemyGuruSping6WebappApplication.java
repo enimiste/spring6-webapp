8e48a18b9b95594e982e07ed4e79b44b2a328723
Enimiste <nouni.elbachir@gmail.com>
2023-04-09 15:32:26 +0200

    Project creation

---
+++ src/main/java/com/guru/udemy/sping6webapp/UdemyGuruSping6WebappApplication.java
@@ -0,0 +1,13 @@
+package com.guru.udemy.sping6webapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UdemyGuruSping6WebappApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UdemyGuruSping6WebappApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/guru/udemy/sping6webapp/UdemyGuruSping6WebappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.guru.udemy.sping6webapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UdemyGuruSping6WebappApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
